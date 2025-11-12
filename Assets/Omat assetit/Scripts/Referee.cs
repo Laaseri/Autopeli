98d66cb7cf31bf58f8d213b914e17683c9d0bd3d
Millainen <79996072+Millainen@users.noreply.github.com>
2025-11-12 19:29:16 +0200

    Tarkistus pusku

--- Assets/Omat assetit/Scripts/Referee.cs
+++ Assets/Omat assetit/Scripts/Referee.cs
@@ -1,11 +1,34 @@
+using UnityEditor.Rendering.BuiltIn.ShaderGraph;
 using UnityEngine;
 
 public class Referee : MonoBehaviour
+
 {
+    private bool winnerDeclared = false;
     private void OnTriggerEnter(Collider car)
     {
         CarIdentify id = car.GetComponent<CarIdentify>();
+
         string winnerName = id.display;
-        Debug.Log($"WINNER: {winnerName}");
+        if(id.kind == CarKind.Player)
+        {
+            var validator = car.GetComponent<Pelaajankierrostarkistus>();
+            if (validator == null)
+            {
+                Debug.LogError("Puuttuu Pelaajankierrostarkistus scripti");
+                return;
+            }
+            if (!validator.AllVisitedThisLap)
+            {
+                Debug.Log("Pelaaja ylitti maaliviivan, mutta kaikki checkpointit eivät ole kunnossa -> ei voittoa");
+                return;
+            }
+        }
+        if(winnerDeclared == false)
+        {
+            winnerDeclared = true;
+            Debug.Log($"WINNER: {winnerName}");    
+        }
+        
     }
 }
