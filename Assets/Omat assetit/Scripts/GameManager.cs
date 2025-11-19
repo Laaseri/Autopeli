9c119b6582a0ee54e7bc796ced6215420a26bb25
Laaseri <lasse.larjo@gmail.com>
2025-11-19 19:43:14 +0200

    Uusia tiedostoja

---
+++ Assets/Omat assetit/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+
+public enum RacePhase{Countdown, Racing, Finished}
+
+public class GameManager : MonoBehaviour
+
+
+
+{
+    public static GameManager Instance {get; private; set; }
+    public RacePhase Phase{ get; get; } = RacePhase.Countdown;
+
+    void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this; 
+        DontDestroyOnLoad(gameObject)
+    }
+}
